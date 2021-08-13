0e6bbdca6ab1fe839366d371908f8c635e2e1718
berkay.ozgen <berkayozgenturkuvaz@gmail.com>
2021-08-13 15:11:01 +0300

    Adding SplashScreen.kt

---
+++ app/src/main/java/com/example/tmdcontactsapp/view/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.example.tmdcontactsapp.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tmdcontactsapp.R
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        imageGif.alpha = 0f
+        imageGif.animate().setDuration(1500).alpha(1f).withEndAction {
+            val i = Intent(this,MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
